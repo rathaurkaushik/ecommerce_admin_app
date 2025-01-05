a267716270b350bd70531bd3e04cd77d438abd7f
kaushik <rathaurkaushik015@gmail.com>
2025-01-05 23:08:27 +0530

    develop lable text and validator

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,36 +1,42 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
   final TextInputType keyboardType;
   final String hintText;
-  final int? maxLines;
-  final TextEditingController? controller;
-  final Function(String)? onChanged;
-  final double borderRadius;
+  final int maxLines;
+  final String labelText; // Fixed parameter name
+  final String? Function(String?)? validator;
 
   const CustomTextField({
     Key? key,
+    required this.controller,
+    required this.labelText, // Fixed usage
+    required this.keyboardType,
     required this.hintText,
-    this.keyboardType = TextInputType.text,
-    this.controller,
-    this.maxLines,
-    this.onChanged,
-    this.borderRadius = 15.0,
+    this.maxLines = 1,
+    this.validator,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-maxLines: maxLines,
-      keyboardType: keyboardType,
+    return TextFormField(
       controller: controller,
-      onChanged: onChanged,
+      keyboardType: keyboardType,
+      maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hintText,
+        labelText: labelText, // Corrected to use the right parameter
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(borderRadius),
+          borderRadius: BorderRadius.circular(12),
         ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.deepPurple, width: 2),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
       ),
+      validator: validator,
     );
   }
 }
